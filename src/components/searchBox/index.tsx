69ec28aea4b5875df5bb9202178f1b09706ddf55
ardotheedu <eduardosalima3@gmail.com>
2021-10-12 14:42:36 -0300

    feat: get city temperature using WeatherAPI

--- src/components/searchBox/index.tsx
+++ src/components/searchBox/index.tsx
@@ -1,14 +1,34 @@
-import { ChangeEvent, FormEvent, useState } from 'react';
+import axios from 'axios';
+import { ChangeEvent, FormEvent, useContext, useState } from 'react';
 import styles from './styles.module.scss';
+import { AuthContext } from '../../context/tracksContext';
+
 
 export function SearchBox() {
     const [searchOption, setSearchOptions] = useState('cidade')
+    const [city, setCity] = useState('')
+
+    const {getTracks} = useContext(AuthContext)
 
     function handleSelectedOption(event: ChangeEvent<HTMLSelectElement>) {
       event.preventDefault;
       setSearchOptions(event.target.value)
-      console.log(event.target.value)
     }
+
+    async function handleSearchWeather() {
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.NEXT_PUBLIC_WHEATER_KEY}`, {
+        params: {
+          units: 'metric'
+        }
+      })
+      console.log(response.data)
+
+      await getTracks()
+
+    }
+
+    
+
     return (
         <div className={styles.container}>
           <div className={styles.searchBox}>
@@ -19,18 +39,18 @@ export function SearchBox() {
               </select>
             </div>
             {searchOption == 'cidade' ? (
-              <div className={styles.searchBarCity}>
-                <input type="text" placeholder="Digite o nome da cidade" />
+              <div className={styles.searchBar}>
+                <input type="text" placeholder="Digite o nome da cidade" value={city} onChange={e => setCity(e.target.value)}/>
               </div>
             ) : (
-              <div className={styles.searchBarCoor}>
+              <div className={styles.searchBar}>
                 <input type="text" placeholder="Digite a latitude" />
                 <input type="text" placeholder="Digite a longitude" />
               </div>  
             )}
           </div>
           <div>
-              <button className={styles.search}>
+              <button className={styles.search} onClick={handleSearchWeather}>
                 Pesquisar
               </button>
           </div>
